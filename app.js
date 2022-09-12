84061007d8d634e5a09936f9fc7af6e760bf21b5
Ashrrith <92615052+Ashrrith@users.noreply.github.com>
2022-09-12 11:24:23 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,162 @@
+if(process.env.NODE_ENV !== "production"){
+  require("dotenv").config();
+}
+
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const ejsMate = require("ejs-mate");
+const methodOverride = require("method-override");
+const ExpressError = require("./utils/ExpressError");
+const session = require("express-session");
+const flash = require("connect-flash");
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
+const helmet = require('helmet');
+const MongoStore = require("connect-mongo");
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/yelp-camp";
+
+//routes
+const campgroundsRoutes = require("./routes/campgrounds");
+const reviewsRoutes = require("./routes/reviews");
+const usersRoutes = require("./routes/users");
+const ExpressMongoSanitize = require("express-mongo-sanitize");
+
+
+
+mongoose
+  .connect(dbUrl)
+  .catch((error) => handleError(error));
+
+const app = express();
+
+
+app.engine('ejs', ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(ExpressMongoSanitize());
+
+const secret = process.env.SECRET || "thisshouldbeabettersecret"
+
+const store = MongoStore.create({
+  mongoUrl: dbUrl,
+  touchAfter: 24 * 60 * 60,
+  crypto: {
+      secret: secret
+  }
+});
+
+store.on('error', function(e){
+  console.log('Session Store Error', e);
+})
+
+const sessionConfig = {
+  store,
+  name: 'sess',
+  secret,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    //secure: true,
+    expires: Date.now() + 1000*60*60*24*7,
+    maxAge: 1000*60*60*24*7
+  }
+}
+
+app.use(session(sessionConfig));
+app.use(flash());
+app.use(helmet());
+
+const scriptSrcUrls = [
+  "https://stackpath.bootstrapcdn.com/",
+  "https://api.tiles.mapbox.com/",
+  "https://api.mapbox.com/",
+  "https://kit.fontawesome.com/",
+  "https://cdnjs.cloudflare.com/",
+  "https://cdn.jsdelivr.net",
+];
+const styleSrcUrls = [
+  "https://kit-free.fontawesome.com/",
+  "https://stackpath.bootstrapcdn.com/",
+  "https://api.mapbox.com/",
+  "https://api.tiles.mapbox.com/",
+  "https://fonts.googleapis.com/",
+  "https://use.fontawesome.com/",
+  "https://cdn.jsdelivr.net",
+];
+const connectSrcUrls = [
+  "https://api.mapbox.com/",
+  "https://a.tiles.mapbox.com/",
+  "https://b.tiles.mapbox.com/",
+  "https://events.mapbox.com/",
+];
+const fontSrcUrls = [];
+app.use(
+  helmet.contentSecurityPolicy({
+      directives: {
+          defaultSrc: [],
+          connectSrc: ["'self'", ...connectSrcUrls],
+          scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+          styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+          workerSrc: ["'self'", "blob:"],
+          objectSrc: [],
+          imgSrc: [
+              "'self'",
+              "blob:",
+              "data:",
+              "https://res.cloudinary.com/dq6on4cmh/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+              "https://images.unsplash.com/",
+          ],
+          fontSrc: ["'self'", ...fontSrcUrls],
+      },
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+app.use((req, res, next)=>{
+  res.locals.currUser = req.user;
+  res.locals.success = req.flash('success');
+  res.locals.error = req.flash('error');
+  next();
+});
+
+
+
+// using the routes
+app.use('/', usersRoutes);
+app.use('/campgrounds', campgroundsRoutes)
+app.use('/campgrounds/:id/reviews', reviewsRoutes)
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+
+app.all('*', (req, res, next) =>{
+    next(new ExpressError('Page Not Found', 404))
+})
+
+app.use((err, req, res, next)=>{
+    const {statusCode = 500} = err;
+    if(!err.message) err.message = 'Oh No, Something Went Wrong'
+    res.status(statusCode).render('error', {err});
+})
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Serving on port ${port}`);
+});
+
+
---
+++ middleware.js
@@ -0,0 +1,63 @@
+const Campground = require("./models/campground");
+const ExpressError = require("./utils/ExpressError");
+const {campgroundSchema, reviewSchema} = require("./schemas");
+const { findById } = require("./models/campground");
+const Review = require('./models/review');
+
+
+module.exports.isLoggedIn = (req, res, next) =>{
+    if(!req.isAuthenticated()){
+        //storing the url before logging in
+        req.session.returnTo = req.originalUrl;
+        req.flash('error', 'You must be signed in!');
+        return res.redirect('/login');
+    }
+    next();
+}
+
+
+module.exports.validateCampground = (req, res, next) =>{
+    const {error} = campgroundSchema.validate(req.body);
+  
+    if(error)
+    {
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg, 400);
+    }
+    else{
+        next();
+    }
+  }
+  
+  module.exports.isAuthor = async (req, res, next)=>{
+    const { id } = req.params;
+      const campground = await Campground.findById(id);
+      if(!campground.author.equals(req.user._id))
+      {
+        req.flash('error', "You don't have permission to this!");
+        return res.redirect(`/campgrounds/${id}`);
+      }
+      next();
+  }
+
+   module.exports.validateReview = (req, res, next) => {
+    const { error } = reviewSchema.validate(req.body);
+  
+    if (error) {
+      const msg = error.details.map((el) => el.message).join(",");
+      throw new ExpressError(msg, 400);
+    } else {
+      next();
+    }
+  };
+
+  module.exports.isReviewAuthor = async (req, res, next)=>{
+    const {id,reviewId} = req.params;
+      const review = await Review.findById(reviewId);
+      if(!review.author.equals(req.user._id))
+      {
+        req.flash('error', "You don't have permission to delete this review!");
+        return res.redirect(`/campgrounds/${id}`);
+      }
+      next();
+  }
